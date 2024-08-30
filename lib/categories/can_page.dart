e9e9b110eff9231179c708c16b2669cd3dbb1650
junhuihi <wnsgml3693@gmail.com>
2024-08-31 04:08:02 +0900

    홈 카테고리 이미지 추가

--- lib/categories/can_page.dart
+++ lib/categories/can_page.dart
@@ -19,80 +19,100 @@ class CanPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xff6AC99F),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0), // 전체 패딩 추가
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                child: Text(
-                  '예시',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+      body: Column(
+        children: [
+          // 이미지 위젯 추가
+          Container(
+            padding: EdgeInsets.only(
+              top: 16.0, // 위쪽에 16픽셀 패딩
+              left: 16.0, // 왼쪽에 16픽셀 패딩
+              right: 16.0, // 오른쪽에 16픽셀 패딩
+              bottom: 0.0, // 아래쪽에는 패딩 없음
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/images/openCan.png', // 이미지 경로
+                height: 200.0, // 원하는 높이로 조정
+                fit: BoxFit.scaleDown, // 이미지 크기에 맞게 조정
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 철캔, 알루미늄캔 등\n'
-                  '• 음료캔, 통조림, 부탄가스, 살충제용기 등',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                child: Text(
-                  '버리는 방법',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 내용물을 비우고 물로 헹구는 등 이물질을 제거하고 배출\n'
-                  '• 플라스틱 뚜껑 등 금속캔과 다른 재질은 제거하고 배출',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, bottom: 8.0),
-                child: Text(
-                  '주의',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 가스용기는 가급적 통풍이 잘되는 장소에서 노즐을 누르는 등 내용물을 완전히 제거한 후 배출',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0), // 전체 패딩 추가
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                      child: Text(
+                        '예시',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 철캔, 알루미늄캔 등\n'
+                        '• 음료캔, 통조림, 부탄가스, 살충제용기 등',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                      child: Text(
+                        '버리는 방법',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 내용물을 비우고 물로 헹구는 등 이물질을 제거하고 배출\n'
+                        '• 플라스틱 뚜껑 등 금속캔과 다른 재질은 제거하고 배출',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, bottom: 8.0),
+                      child: Text(
+                        '주의',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 가스용기는 가급적 통풍이 잘되는 장소에서 노즐을 누르는 등 내용물을 완전히 제거한 후 배출',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/categories/paper_page.dart
+++ lib/categories/paper_page.dart
@@ -19,78 +19,100 @@ class PaperPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: const Color(0xff6AC99F),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(30.0), // 전체 패딩 추가
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                child: Text(
-                  '예시',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
+      body: Column(
+        children: [
+          // 이미지 위젯 추가
+          Container(
+            padding: EdgeInsets.only(
+              top: 16.0, // 위쪽에 16픽셀 패딩
+              left: 16.0, // 왼쪽에 16픽셀 패딩
+              right: 16.0, // 오른쪽에 16픽셀 패딩
+              bottom: 0.0, // 아래쪽에는 패딩 없음
+            ),
+            child: Center(
+              child: Image.asset(
+                'assets/images/openPaper.png', // 이미지 경로
+                height: 200.0, // 원하는 높이로 조정
+                fit: BoxFit.scaleDown, // 이미지 크기에 맞게 조정
               ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 신문, 책, 노트, 상자류, 골판지 등',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                child: Text(
-                  '버리는 방법',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 종이류와 다른 재질(테이프, 송장, 스프링 등)은 제거한 후 배출',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, bottom: 8.0),
-                child: Text(
-                  '주의',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
-                child: Text(
-                  '• 다른 재질과 혼합 구성된 종이는 종량제 봉투에 배출 (영수증, 금(은)박지, 부직포 등)',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.normal,
-                  ),
-                ),
-              ),
-            ],
+            ),
           ),
-        ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0), // 전체 패딩 추가
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // 왼쪽 정렬
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                      child: Text(
+                        '예시',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 신문, 책, 노트, 상자류, 골판지 등',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                      child: Text(
+                        '버리는 방법',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 종이류와 다른 재질(테이프, 송장, 스프링 등)은 제거한 후 배출',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, bottom: 8.0),
+                      child: Text(
+                        '주의',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0), // 왼쪽 여백 추가
+                      child: Text(
+                        '• 다른 재질과 혼합 구성된 종이는 종량제 봉투에 배출 (영수증, 금(은)박지, 부직포 등)',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
